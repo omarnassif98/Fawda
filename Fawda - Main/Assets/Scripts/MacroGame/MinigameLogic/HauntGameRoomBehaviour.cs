e28fd047220bc6457be93465171870ff4c0527e0
Omar Abou Nassif Mourad <omar.nassif98@gmail.com>
2024-02-11 22:17:26 +0200

    Progress on Wave Function Collapse

--- Fawda - Main/Assets/Scripts/MacroGame/MinigameLogic/HauntGameRoomBehaviour.cs
+++ Fawda - Main/Assets/Scripts/MacroGame/MinigameLogic/HauntGameRoomBehaviour.cs
@@ -3,32 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-    enum AnchorCode {
-        BOTTOM_LEFT=-1,
-        TOP_RIGHT=1,
-        MIDDLE=0
-    }
-    
-public class HauntGameRoomBehaviour : MonoBehaviour
-{   
-    static int num = 0;
-    static bool extentReached;
-    static float width_used = 0, height_used = 0;
-    const float FLOOR_THICKNESS = 0.2f, WALL_THICKNESS = 0.4f, MAX_MAP_HEIGHT = 150, MAX_MAP_WIDTH = 70;
-    float targetOpacity = 1;
-    Renderer[] bottomWallMats = null; //WHO THE FUCK?????????
-    short occupancy = 0;
-    float offset;
-    // Start is called before the first frame update
-
     public enum AnchorCode{
         TOP=1,
         BOTTOM=-1,
         LEFT=-2,
         RIGHT=2
     }
-    static Dictionary<AnchorCode,float> anchorExtents = new Dictionary<AnchorCode, float>();
-    public struct WallAnchor{
+
+        public struct WallAnchor{
+            public static readonly WallAnchor Empty;
             public Vector3 anchorPoint;
             public AnchorCode direction;
             public HauntGameRoomBehaviour neighbor;
@@ -46,6 +29,19 @@ public class HauntGameRoomBehaviour : MonoBehaviour
 
             
         }
+    
+public class HauntGameRoomBehaviour : MonoBehaviour
+{   
+    static int num = 0;
+    static bool extentReached;
+    static float width_used = 0, height_used = 0, view_angle_factor = 1;
+    const float FLOOR_THICKNESS = 0.2f, MAX_MAP_HEIGHT = 150, MAX_MAP_WIDTH = 70, OFFSET = FLOOR_THICKNESS + 5.0f/2;
+    float targetOpacity = 1;
+    readonly Renderer[] bottomWallMats = null; //WHO THE FUCK?????????
+    short occupancy = 0;
+    // Start is called before the first frame update
+    static readonly Dictionary<AnchorCode,float> anchorExtents = new Dictionary<AnchorCode, float>();
+
     List<WallAnchor> doors = new List<WallAnchor>();
 
 
@@ -55,6 +51,9 @@ public class HauntGameRoomBehaviour : MonoBehaviour
         anchorExtents[AnchorCode.RIGHT] = 0.5f;
         anchorExtents[AnchorCode.LEFT] = -0.5f;
         anchorExtents[AnchorCode.BOTTOM] = -0.5f;
+        float viewAngle = Camera.main.transform.eulerAngles.x;
+        view_angle_factor = 1/Mathf.Sin(Mathf.Deg2Rad * viewAngle);
+        print("VIEW ANGLE FACTOR = " + view_angle_factor + " - from: " + viewAngle);
     }
     void FixedUpdate(){
         if(bottomWallMats == null) return;
@@ -65,43 +64,24 @@ public class HauntGameRoomBehaviour : MonoBehaviour
     }
 
     public void Generate(){
-        DetermineSize();
         DetermineLocation();
         SetupFloorBounds();
         GenerateDoors();
-    }
-
-    public void FeedWallAnchor(WallAnchor _anchorRelativeToNeighbor, HauntGameRoomBehaviour _neighbor){
-        AnchorCode codeRelativeToSelf = FlipAnchorCode(_anchorRelativeToNeighbor.direction);
-        WallAnchor fedWallAnchor = new WallAnchor(_anchorRelativeToNeighbor.anchorPoint,codeRelativeToSelf,_neighbor);
-        doors.Add(fedWallAnchor);
     }
 
     private AnchorCode FlipAnchorCode(AnchorCode _anch){
         return (AnchorCode)(-1 * (int)_anch);
     }
 
-    void DetermineSize(int _forceRand = -1){
-        Vector3 roomSize = Vector3.zero;
-         switch ((_forceRand == -1)?UnityEngine.Random.Range(0,4):_forceRand){
-            case 0:
-                roomSize = new Vector3(3,5,25);
-                break;
-            case 1:
-                roomSize = new Vector3(12,5,10);
-                break;
-            case 2:
-                roomSize = new Vector3(10,5,6);
-                break;
-            case 3:
-                roomSize = new Vector3(4,5,12);
-                break;
-        }
+    public void DetermineSize(float _xWidth, float _zHeight){
+        Vector3 roomSize = new Vector3(_xWidth, 5, _zHeight);
+        if(doors.Count > 0 && (doors[0].direction == AnchorCode.LEFT || doors[0].direction == AnchorCode.RIGHT)) roomSize = new Vector3(_zHeight,5,_xWidth);
+        
+        roomSize.z *= view_angle_factor;
         width_used += roomSize.x;
         height_used += roomSize.z;
         transform.localScale = roomSize;
         extentReached = width_used > MAX_MAP_WIDTH || height_used > MAX_MAP_HEIGHT;
-        offset = (transform.localScale.y/2 + FLOOR_THICKNESS/2);
     }
 
     void DetermineLocation(){
@@ -122,10 +102,12 @@ public class HauntGameRoomBehaviour : MonoBehaviour
                 break;
         }
         Vector3 prospect = new Vector3(doors[0].anchorPoint.x, transform.position.y, doors[0].anchorPoint.z) + dir * factor/2 * Mathf.Sign((int)FlipAnchorCode(doors[0].direction));
-        foreach(Collider col in Physics.OverlapBox(prospect,transform.localScale/2,transform.rotation)){
+        foreach(Collider col in Physics.OverlapBox(prospect,transform.localScale,transform.rotation)){
             if(col.GetComponent<HauntGameRoomBehaviour>() && col.gameObject != doors[0].neighbor.gameObject && col.gameObject != gameObject){ // UNRELIABLE
                 print(string.Format("Graceful Deletion - {0}", col.gameObject.name));
-                Destroy(gameObject);}
+                extentReached = true;
+                Destroy(gameObject);
+                }
         }
         transform.position = prospect;
     }
@@ -134,7 +116,7 @@ public class HauntGameRoomBehaviour : MonoBehaviour
         BoxCollider roomCol = gameObject.AddComponent<BoxCollider>();
         roomCol.center = transform.position;
         roomCol.size = Vector3.one;
-        transform.position = transform.position + Vector3.up * offset;
+        transform.position = transform.position + Vector3.up * OFFSET;
         roomCol.isTrigger = true;
         gameObject.layer = 2;
         gameObject.name = "Room " + num;
@@ -142,7 +124,7 @@ public class HauntGameRoomBehaviour : MonoBehaviour
         GameObject floor = GameObject.CreatePrimitive(PrimitiveType.Cube);
         floor.name = "Floor";
         floor.transform.localScale = new Vector3(transform.localScale.x,FLOOR_THICKNESS,transform.localScale.z);
-        floor.transform.position = transform.position + Vector3.down * offset;
+        floor.transform.position = transform.position + Vector3.down * OFFSET;
         floor.transform.parent = transform;
         Material floorMat = (Material)Resources.Load("MinigameAssets/Haunt/FloorMaterial");
         floor.GetComponent<Renderer>().material = floorMat;
@@ -154,12 +136,8 @@ public class HauntGameRoomBehaviour : MonoBehaviour
             if (UnityEngine.Random.Range(0,5) < 3) continue;  
             float xAnchor = wallDirection == AnchorCode.BOTTOM || wallDirection == AnchorCode.TOP ? UnityEngine.Random.Range(-0.25f,0.25f):anchorExtents[wallDirection];
             float zAnchor = wallDirection == AnchorCode.LEFT || wallDirection == AnchorCode.RIGHT ? UnityEngine.Random.Range(-0.25f,0.25f):anchorExtents[wallDirection];
-            Vector3 doorPos = transform.position + Vector3.down * offset + Vector3.right * xAnchor * transform.localScale.x + Vector3.forward * zAnchor * transform.localScale.z;
+            Vector3 doorPos = transform.position + Vector3.down * OFFSET + Vector3.right * xAnchor * transform.localScale.x + Vector3.forward * zAnchor * transform.localScale.z;
             WallAnchor anch = new WallAnchor(doorPos,wallDirection);
-            anch.neighbor = new GameObject().AddComponent<HauntGameRoomBehaviour>();
-            anch.neighbor.transform.parent = transform.parent;
-            anch.neighbor.FeedWallAnchor(anch,this);
-            anch.neighbor.Generate();
             VisualizeAnchor(anch);
             doors.Add(anch);
         }
@@ -179,4 +157,16 @@ public class HauntGameRoomBehaviour : MonoBehaviour
         go.transform.position = _anchor.anchorPoint;
         go.transform.parent = transform;
     }
+
+    public WallAnchor[] GetAnchors(){
+        return doors.ToArray();
+    }
+
+    public void FeedWallAnchor(WallAnchor _anchorRelativeToNeighbor, HauntGameRoomBehaviour _neighbor){
+        AnchorCode codeRelativeToSelf = FlipAnchorCode(_anchorRelativeToNeighbor.direction);
+        WallAnchor fedWallAnchor = new WallAnchor(_anchorRelativeToNeighbor.anchorPoint,codeRelativeToSelf,_neighbor);
+        doors.Add(fedWallAnchor);
+    }
+
+    
 }
--- Fawda - Main/Assets/Scripts/MacroGame/MinigameLogic/WaveCollapseGenerator.cs
+++ Fawda - Main/Assets/Scripts/MacroGame/MinigameLogic/WaveCollapseGenerator.cs
@@ -6,20 +6,25 @@ using UnityEngine;
 public class WaveCollapseGenerator : MonoBehaviour
 {
     Dictionary<string, RoomNode> graph = new Dictionary<string, RoomNode>();
-    struct RoomNodeEdge{
+    public struct RoomNodeEdge{
         public RoomNode neighbor;
         public int weight;
-        public RoomNodeEdge(RoomNode _neighbor, int _weight){
+        public bool walled;
+        public RoomNodeEdge(RoomNode _neighbor, int _weight, bool _walled){
             neighbor = _neighbor;
             weight = _weight;
+            walled = _walled;
         }
     }
 
-    struct RoomNode{
-        float xWidth;
-        float zHeigth;
-        List<RoomNodeEdge> connections;
-        public RoomNode(float _xWidth, float _zHeight){
+    public struct RoomNode{
+        public static readonly RoomNode Empty;
+        public float xWidth;
+        public float zHeigth;
+        public string name;
+       public List<RoomNodeEdge> connections;
+        public RoomNode(string _name, float _xWidth, float _zHeight){
+            name = _name;
             xWidth = _xWidth;
             zHeigth = _zHeight;
             connections = new List<RoomNodeEdge>();
@@ -32,32 +37,67 @@ public class WaveCollapseGenerator : MonoBehaviour
     }
 
     void LoadConfig(){
+        
         TextAsset rawNodeData = Resources.Load<TextAsset>("MinigameAssets/Haunt/WaveFunctionRoomNodes");
-        string[] rows = rawNodeData.text.Split('\n');
-        for(int i = 1; i < rows.Length; i++){
-            string row = rows[i];
+        string[] nodeRows = rawNodeData.text.Split('\n');
+        for(int i = 1; i < nodeRows.Length; i++){
+            string row = nodeRows[i];
             try
             {
                 string[] vals = row.Split(',');
-                graph[vals[0]] = new RoomNode(float.Parse(vals[1].Trim()), float.Parse(vals[2].Trim()));
+                graph[vals[0].Trim()] = new RoomNode(vals[0].Trim(), float.Parse(vals[1].Trim()), float.Parse(vals[2].Trim()));
                 print("Node Created: " + vals[0]);
             }
             catch (System.Exception)
             {
                 Debug.LogError("Error Loading Node: " + row);
             }
+        }
 
+        TextAsset rawEdgeData = Resources.Load<TextAsset>("MinigameAssets/Haunt/WaveFunctionNodeEdges");
+        string[] edgeRows = rawEdgeData.text.Split('\n');
+        for(int i = 1; i<edgeRows.Length; i++){
+            string row = edgeRows[i];
+            try
+            {
+                string[] vals = row.Split(',');
+                graph[vals[0].Trim()].connections.Add(new RoomNodeEdge(graph[vals[1].Trim()], int.Parse(vals[2].Trim()), bool.Parse(vals[3].Trim())));
+                print("Edge Created: " + vals[0] + " ->> " + vals[1]);
+            }
+            catch (System.Exception)
+            {
+                Debug.LogError("Error Loading Edge: " + row);
+            }
         }
     }
 
     void Start(){
-        //GenerateRoom();
+        GenerateRoom(graph["square"],WallAnchor.Empty);
     }
-    void GenerateRoom(){
+    void GenerateRoom(RoomNode _preset, WallAnchor _wallAnchor, HauntGameRoomBehaviour _prevRoom = null){
         HauntGameRoomBehaviour room = new GameObject().AddComponent<HauntGameRoomBehaviour>();
         room.transform.parent = transform;
-        room.transform.position = Vector3.zero;
+        if (_prevRoom) room.FeedWallAnchor(_wallAnchor,_prevRoom);
+        room.DetermineSize(_preset.xWidth, _preset.zHeigth);
         room.Generate();
+        foreach(WallAnchor wa in room.GetAnchors()){
+            if(wa.neighbor == null) GenerateRoom(PickRandomNeighborTemplate(_preset),wa,room);
+        }
+    }
+
+    RoomNode PickRandomNeighborTemplate(RoomNode _previousNode){
+        int[] weightPool = new int[_previousNode.connections.Count];
+        for(int i = 0; i < weightPool.Length; i++){
+            weightPool[i] = (i==0?0:weightPool[i-1]) + _previousNode.connections[i].weight;
+        }
+
+        int randChoice = UnityEngine.Random.Range(0,weightPool[weightPool.Length-1]);
+
+        for(int i = 0; i < weightPool.Length; i++){
+            if(randChoice < weightPool[i]) return _previousNode.connections[i].neighbor;
+        }
+
+        return RoomNode.Empty;        
     }
 
 
